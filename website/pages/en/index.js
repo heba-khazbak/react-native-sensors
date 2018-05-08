fd83c03f6d4f032e71019c2982fde8b7e522efc3
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-05-08 23:03:08 +0200

    apply prettier styles

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -15,152 +15,152 @@ const GridBlock = CompLibrary.GridBlock;
 const siteConfig = require(process.cwd() + "/siteConfig.js");
 
 function imgUrl(img) {
-	return siteConfig.baseUrl + "img/" + img;
+  return siteConfig.baseUrl + "img/" + img;
 }
 
 function docUrl(doc, language) {
-	return siteConfig.baseUrl + "docs/" + (language ? language + "/" : "") + doc;
+  return siteConfig.baseUrl + "docs/" + (language ? language + "/" : "") + doc;
 }
 
 function pageUrl(page, language) {
-	return siteConfig.baseUrl + (language ? language + "/" : "") + page;
+  return siteConfig.baseUrl + (language ? language + "/" : "") + page;
 }
 
 class Button extends React.Component {
-	render() {
-		return (
-			<div className="pluginWrapper buttonWrapper">
-				<a className="button" href={this.props.href} target={this.props.target}>
-					{this.props.children}
-				</a>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={this.props.href} target={this.props.target}>
+          {this.props.children}
+        </a>
+      </div>
+    );
+  }
 }
 
 Button.defaultProps = {
-	target: "_self"
+  target: "_self"
 };
 
 const SplashContainer = props => (
-	<div className="homeContainer">
-		<div className="homeSplashFade">
-			<div className="wrapper homeWrapper">{props.children}</div>
-		</div>
-	</div>
+  <div className="homeContainer">
+    <div className="homeSplashFade">
+      <div className="wrapper homeWrapper">{props.children}</div>
+    </div>
+  </div>
 );
 
 const Logo = props => (
-	<div className="projectLogo">
-		<img src={props.img_src} />
-	</div>
+  <div className="projectLogo">
+    <img src={props.img_src} />
+  </div>
 );
 
 const ProjectTitle = props => (
-	<h2 className="projectTitle">
-		{siteConfig.title}
-		<small>{siteConfig.tagline}</small>
-	</h2>
+  <h2 className="projectTitle">
+    {siteConfig.title}
+    <small>{siteConfig.tagline}</small>
+  </h2>
 );
 
 const PromoSection = props => (
-	<div className="section promoSection">
-		<div className="promoRow">
-			<div className="pluginRowBlock">{props.children}</div>
-		</div>
-	</div>
+  <div className="section promoSection">
+    <div className="promoRow">
+      <div className="pluginRowBlock">{props.children}</div>
+    </div>
+  </div>
 );
 
 class HomeSplash extends React.Component {
-	render() {
-		let language = this.props.language || "";
-		return (
-			<SplashContainer>
-				<div className="inner">
-					<ProjectTitle />
-					<PromoSection>
-						<ul style={{textAlign: 'left'}}>
-							<li>Accelerometer</li>
-							<li>Gyroscope</li>
-							<li>Magnetometer</li>
-						</ul>
-					</PromoSection>
-					<Button href="#try">Try It Out</Button>
-				</div>
-			</SplashContainer>
-		);
-	}
+  render() {
+    let language = this.props.language || "";
+    return (
+      <SplashContainer>
+        <div className="inner">
+          <ProjectTitle />
+          <PromoSection>
+            <ul style={{ textAlign: "left" }}>
+              <li>Accelerometer</li>
+              <li>Gyroscope</li>
+              <li>Magnetometer</li>
+            </ul>
+          </PromoSection>
+          <Button href="#try">Try It Out</Button>
+        </div>
+      </SplashContainer>
+    );
+  }
 }
 
 const Block = props => (
-	<Container
-		padding={["bottom", "top"]}
-		id={props.id}
-		background={props.background}
-	>
-		<GridBlock align="center" contents={props.children} layout={props.layout} />
-	</Container>
+  <Container
+    padding={["bottom", "top"]}
+    id={props.id}
+    background={props.background}
+  >
+    <GridBlock align="center" contents={props.children} layout={props.layout} />
+  </Container>
 );
 
 const Features = props => (
-	<Block layout="fourColumn">
-		{[
-			{
-				image: 'https://dwglogo.com/wp-content/uploads/2017/05/RxJS_logo.png',
-				imageAlign: "top",
-				title: "RxJS Observables as API",
-				content: "This gives you an intuitive way to deal with sensor data"
-			},
-			{
-				image: 'http://amenssolutions.com/images/HD/ios-android.png',
-				imageAlign: "top",
-				title: "Full iOS & Android support",
-				content:
-					"Our philosophy is not to ship partial support; same sensors for every device"
-			}
-		]}
-	</Block>
+  <Block layout="fourColumn">
+    {[
+      {
+        image: "https://dwglogo.com/wp-content/uploads/2017/05/RxJS_logo.png",
+        imageAlign: "top",
+        title: "RxJS Observables as API",
+        content: "This gives you an intuitive way to deal with sensor data"
+      },
+      {
+        image: "http://amenssolutions.com/images/HD/ios-android.png",
+        imageAlign: "top",
+        title: "Full iOS & Android support",
+        content:
+          "Our philosophy is not to ship partial support; same sensors for every device"
+      }
+    ]}
+  </Block>
 );
 
 const Showcase = props => {
-	if ((siteConfig.users || []).length === 0) {
-		return null;
-	}
+  if ((siteConfig.users || []).length === 0) {
+    return null;
+  }
 
-	return (
-		<div className="productShowcaseSection paddingBottom">
-			<h2>{"Who's Using This?"}</h2>
-			<p>This project is used by all these people</p>
-			<Block layout="fourColumn">
-				{siteConfig.users
-					.filter(user => {
-						return user.pinned;
-					}).slice(0, 4)
-					.map(user => ({
-						image: user.image,
-						imageAlign: "top",
-						title: user.caption
-					})
-				)}
-			</Block>
-		</div>
-	);
+  return (
+    <div className="productShowcaseSection paddingBottom">
+      <h2>{"Who's Using This?"}</h2>
+      <p>This project is used by all these people</p>
+      <Block layout="fourColumn">
+        {siteConfig.users
+          .filter(user => {
+            return user.pinned;
+          })
+          .slice(0, 4)
+          .map(user => ({
+            image: user.image,
+            imageAlign: "top",
+            title: user.caption
+          }))}
+      </Block>
+    </div>
+  );
 };
 
 class Index extends React.Component {
-	render() {
-		let language = this.props.language || "";
+  render() {
+    let language = this.props.language || "";
 
-		return (
-			<div>
-				<HomeSplash language={language} />
-				<div className="mainContainer">
-					<Features />
-					<Showcase language={language} />
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <HomeSplash language={language} />
+        <div className="mainContainer">
+          <Features />
+          <Showcase language={language} />
+        </div>
+      </div>
+    );
+  }
 }
 
 module.exports = Index;
